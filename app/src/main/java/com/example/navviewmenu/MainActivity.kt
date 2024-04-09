d99eb19242c34783666609a963047901038de8e0
Frank <f.neumann.berlin@googlemail.com>
2024-04-09 11:15:15 +0200

    mit funktionierender Logig

--- app/src/main/java/com/example/navviewmenu/MainActivity.kt
+++ app/src/main/java/com/example/navviewmenu/MainActivity.kt
@@ -1,17 +1,68 @@
 package com.example.navviewmenu
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import com.example.navviewmenu.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        with(binding.toolbar){
+            title = "Meine App"
+            setTitleTextColor(Color.WHITE)
+            setSupportActionBar(this)
+        }
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        toggle = ActionBarDrawerToggle(this,binding.main,R.string.open,R.string.close)
+        binding.main.addDrawerListener(toggle)
+        toggle.syncState()
+
+        binding.navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.nav_home -> Toast.makeText(this, "Auf Home", Toast.LENGTH_SHORT).show()
+                R.id.nav_send -> Toast.makeText(this, "Auf Send", Toast.LENGTH_SHORT).show()
+
+            }
+            binding.main.closeDrawer(GravityCompat.START)
+            true
+        }
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
